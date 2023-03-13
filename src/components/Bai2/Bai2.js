770828d69ee53c9f4fdf053556a6fce9834b8bb3
AnhKha22072003 <dorakha14@gmail.com>
2023-03-13 14:41:19 +0700

    create craco

---
+++ src/components/Bai2/Bai2.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+import { Input,Button,Modal } from 'antd';
+const Login=(props)=>
+{
+const [user,setUser]=useState([{email:"anhkha",pass:"kha123"},{email:"khakha",pass:"kha11"}])
+const [open,setOpen]=useState(false)
+const onChange=(e)=>
+  {
+    const name = e.target.name
+    const value = e.target.value
+   
+  }
+const onSubmit=()=>
+{
+
+}
+
+
+    return(
+        <div>
+            <div>
+                <Input name="email" value={user.email} onChange={onChange}></Input>
+                <Input name="pass" value={user.pass} onChange={onChange}></Input>
+                <Button onClick={onSubmit}></Button>
+                <Modal>
+                    <div>
+                        ddang nhap thanh cong
+                    </div>
+                </Modal>
+            </div>
+        </div>
+    )
+}
